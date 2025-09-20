c89f96683291f1dd63a74562b39cba3c53982c65
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-21 01:11:54 +0800

    lesson 5.2 import

---
+++ Lesson 5.2 - Exercise/script.js
@@ -0,0 +1,14 @@
+//declare ur name
+let name = 'Adelle';
+console.log(`my name is ${name}`);
+//declare prices
+let coffee = 5, bagel = 3, soup = 9;
+//calculate cost
+cost = coffee + (bagel *= 2) + soup;
+console.log(`Cost of food ${cost}"`);
+//calculate tax
+tax = cost * 0.1;
+console.log(`Tax is ${tax}"`);
+// calculate total
+total = cost + tax;
+console.log(`Total is ${total}"`);
