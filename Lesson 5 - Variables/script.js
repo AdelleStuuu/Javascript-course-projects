6d88d8108802f0c258373a32b34d76174aecc685
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-21 01:08:45 +0800

    lesson 5 import

---
+++ Lesson 5 - Variables/script.js
@@ -0,0 +1,33 @@
+// let means that the variable is changeable 
+let variable1 = 3;
+console.log(variable1);
+// const means that the variable is not changeable
+const variable2 = 4;
+console.log(variable2);
+
+// this shows that you can perform calculations inside a variable
+let calculation = 2 + 2;
+console.log('did you know that 2+2 is: ', calculation);
+// you can also perform calculations inside the console.log
+console.log(calculation + 2);
+
+// this shows that you can change the value of a variable
+variable1 = variable1 + 1;
+console.log('the new value is: ', variable2);
+
+/* do not:
+-- avoid using var when starting declaring variables
+-- don't use special keywords as variable names
+-- don't use numbers as the first character of a variable name
+-- don't use spaces in variable names
+-- don't use special characters in variable names
+*/
+
+// this shows that you can change the value of a variable
+variable1 = 5;
+console.log(variable1);
+/* how this works:
+1. do not use let because it makes a new variable
+2. use the variable name to change the value of the variable
+3. insert the new value after the equal sign
+*/
