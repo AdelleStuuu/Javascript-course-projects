45681821e2950522ea2f2e3e2db024af160584e8
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-21 01:14:18 +0800

    Lesson 6 import

---
+++ Lesson 6 - Booleans/script.js
@@ -0,0 +1,24 @@
+//this boolean value returns to true
+console.log(3 < 5);
+
+//this boolean value returns to false
+console.log(3 > 5);
+
+//boolean values are written as true or false
+true
+false
+//do not put quotes on booleans
+
+/* comparison operators:
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+=== equal to
+!== not equal to
+*/
+
+//false because one is integer and the other is a string
+console.log(5 === '5');
+//true because it is converted and equal to each other
+console.log(5 == '5');
